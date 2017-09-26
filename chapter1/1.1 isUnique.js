e5fcf077c378e96b3130a1265b96379ee5a295eb
yousifAlneamy <yousif.aladdin@gmail.com>
2017-09-26 22:33:44 +0300

    Add the follow up solution

--- chapter1/1.1 isUnique.js
+++ chapter1/1.1 isUnique.js
@@ -25,3 +25,26 @@ try {
 } catch (e) {
 	console.log(e);
 }
+
+//What if you cannot use additional data structures?
+function isUnique2(str) {
+	if (typeof str !== "string") {
+		throw { Error: "Passed argument should be of type string" };
+	}
+	let checker = 0;
+	for (let i = 0; i < str.length; i++) {
+		let val = str.charCodeAt(i) - 97; // 'a' = 97
+		if ((checker & (1 << val)) > 0) {
+			return false;
+		}
+		checker |= 1 << val;
+	}
+	return true;
+}
+
+console.log(isUnique2("ali"));
+try {
+	console.log(isUnique2(20));
+} catch (e) {
+	console.log(e);
+}
